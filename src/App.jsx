49f276d763cdd5d733471a925d502c179a387b55
michelelongo97 <m.longo7x@gmail.com>
2025-02-04 14:26:15 +0100

    feat: add milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+
 export default function App() {
-  return <input type="text" />;
+  const [article, setArticle] = useState("");
+
+  const handleField = (e) => {
+    setArticle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Inviato");
+    console.log("l'articolo è" + article);
+  };
+
+  return (
+    <>
+      <h1>Lista di articoli</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={article}
+          onChange={handleField}
+          placeholder="Scrivi l'articolo"
+        />
+        <button type="submit">Invia</button>
+        {article && <p>L'articolo è: {article}</p>}
+      </form>
+    </>
+  );
 }
