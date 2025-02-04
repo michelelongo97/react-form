45f6b9ac65766c7649ad6c32a7ce8cd3a28117ab
michelelongo97 <m.longo7x@gmail.com>
2025-02-04 15:07:30 +0100

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [article, setArticle] = useState("");
+  const [articleList, setArticleList] = useState(["articolo 1"]);
 
   const handleField = (e) => {
     setArticle(e.target.value);
@@ -11,12 +12,20 @@ export default function App() {
     e.preventDefault();
     alert("Inviato");
     console.log("l'articolo è" + article);
+    const newArticleList = [...articleList, article];
+    setArticleList(newArticleList);
   };
 
   return (
     <>
       <h1>Lista di articoli</h1>
+      <ul>
+        {articleList.map((article, index) => (
+          <li key={index}>{article}</li>
+        ))}
+      </ul>
       <form onSubmit={handleSubmit}>
+        <h3>Aggiungi articolo</h3>
         <input
           type="text"
           value={article}
@@ -24,7 +33,7 @@ export default function App() {
           placeholder="Scrivi l'articolo"
         />
         <button type="submit">Invia</button>
-        {article && <p>L'articolo è: {article}</p>}
+        {article && <p>L'articolo che stai aggiungendo è: {article}</p>}
       </form>
     </>
   );
